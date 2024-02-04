147431beb0e887b366427a4f08c38308659784f5
iamaman012 <111675545+iamaman012@users.noreply.github.com>
2024-02-04 16:03:08 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Landing from "./components/Landing.js";
+import Maincontainer from "./components/Maincontainer.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./context/movieContext.js";
+import UpdateMovie from "./components/UpdateMovie.js";
+// import UpdateMovie from "./pages/updateMovie.js";
+// import UpdateMovie from './components/UpdateMovie.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <MovieProvider>
+          <Landing>
+            <Routes>
+              <Route path="/" element={<Maincontainer />} />
+              <Route path="/update/:id" element={<UpdateMovie/>}/>
+            </Routes>
+          </Landing>
+        </MovieProvider>
+      </BrowserRouter>
     </div>
   );
 }
